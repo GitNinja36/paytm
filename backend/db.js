f7f63ec9742bc1e9b7bd1bb70592a606f8238a0e
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2025-01-12 01:33:58 +0530

    initializing mini project

---
+++ backend/db.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URL);
+console.log("mongoDB connected");
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+    User
+}
--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,14 @@
 const express = require("express");
- 
+const app = express();
+const mainRouter = require("./routes/index");
+const cors = require("cors");
+
+app.use(cors);
+
+app.use("/api/vi", mainRouter);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`port is working on ${port}`)
+})
